d87d8cc138e110120db37c5edb4bfac63423ee9e
Milinda Kaushika <milindakaushika@outlook.com>
2023-01-14 01:08:04 -0800

    Add js code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
+// Mobile responsive naverbar sticky
 const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", window.scrollY > 100);
 });
+
+// Mobile Resposive naver bar javascript
+let menu = document.querySelector('#menu-icon');
+let naverbar = document.querySelector('.naverbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    naverbar.classList.toggle('open');
+};
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    naverbar.classList.remove('open');
+}
